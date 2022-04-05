d5a7d6cb2ac5263a93028a6b7440f13344d10f14
melanch0lic <kudza76@mail.ru>
2022-04-05 13:33:39 +0300

    put the stock model in a separate file

---
+++ lib/models/stock.dart
@@ -0,0 +1,13 @@
+class StockItem {
+  double price;
+  final String symbol;
+  double volume;
+  final String description;
+
+  StockItem({
+    this.price = 0,
+    this.symbol = 'No data',
+    this.volume = 0,
+    this.description = 'No data',
+  });
+}
