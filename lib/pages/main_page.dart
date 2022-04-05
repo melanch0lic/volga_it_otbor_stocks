56f8824afc5827b69b4ad70c73f5f9660546d90a
melanch0lic <kudza76@mail.ru>
2022-04-05 13:51:05 +0300

    changed and tweaked the appearance of the application, added the appbar

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,71 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-import 'package:test_stock_app/providers/stock_provider.dart';
-import 'package:test_stock_app/widgets/stock_list.dart';
+
+import '../providers/stock_provider.dart';
+import '../widgets/stock_list.dart';
 
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final stockProvider = Provider.of<StockProvider>(context);
     if (!stockProvider.isFetched) {
-      stockProvider.fetchDataFromApi();
+      stockProvider
+          .fetchDataFromApi(); // вставить при инициализации конструктора StockProvider
       // stockProvider.listenStock();
       stockProvider.isFetched = true;
     }
     return Scaffold(
-        backgroundColor: Colors.black,
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.wb_sunny_rounded,
+                  color: Colors.white,
+                ))
+          ],
+          backgroundColor: Colors.grey[800],
+          title: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Center(
-                child: Text('Stocks',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 36,
-                      fontWeight: FontWeight.bold,
-                    )),
-              ),
+              const Text('Stocks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  )),
               Text(
-                DateFormat.yMMMMEEEEd().format(DateTime.now()),
+                DateFormat.yMMMMEEEEd()
+                    .format(DateTime.now()), // поменять отображение времени
                 style: TextStyle(
                   color: Colors.grey[500],
-                  fontSize: 30,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: SizedBox(
-                    height: 50,
-                    child: TextField(
-                      onChanged: (value) {
-                        stockProvider.searchHandler(value);
-                      },
-                      decoration: InputDecoration(
-                          hintStyle: TextStyle(color: Colors.grey[500]),
-                          hintText: "Search",
-                          prefix: const Icon(
-                            Icons.search,
-                          ),
-                          fillColor: Colors.grey[800],
-                          filled: true,
-                          border: const OutlineInputBorder(
-                            borderSide: BorderSide(
-                              width: 0,
-                              style: BorderStyle.none,
-                            ),
-                            borderRadius: BorderRadius.all(Radius.circular(16)),
-                          )),
-                    )),
-              ),
-              SizedBox(
-                  height: MediaQuery.of(context).size.height - 210,
-                  child: StockList()),
             ],
           ),
+        ),
+        // поменять на другой виджет
+        backgroundColor: Colors.black,
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+              child: SizedBox(
+                  height: 50,
+                  child: TextField(
+                    onChanged: (value) {
+                      stockProvider.searchHandler(value);
+                    },
+                    decoration: InputDecoration(
+                        hintStyle:
+                            TextStyle(color: Colors.grey[500], fontSize: 16),
+                        hintText: "Search",
+                        prefixIcon: const Icon(
+                          // отцентрировать Иконку
+                          Icons.search,
+                        ),
+                        fillColor: Colors.grey[800],
+                        filled: true,
+                        border: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(16)),
+                        )),
+                  )),
+            ),
+            Expanded(
+              child: StockList(),
+            ),
+          ],
         ));
   }
 }
