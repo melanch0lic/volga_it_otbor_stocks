c915aeaa4c7a4c6489fedd8e701e298ed6ed1fd1
melanch0lic <kudza76@mail.ru>
2022-04-05 14:57:33 +0300

    moved stock widget to a separate file, changed stock generation

--- lib/widgets/stock_list.dart
+++ lib/widgets/stock_list.dart
@@ -1,113 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:test_stock_app/providers/stock_provider.dart';
+
+import '../providers/stock_provider.dart';
+import '../widgets/stock_item.dart';
 
 class StockList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final stockProvider = Provider.of<StockProvider>(context);
+    final provider = Provider.of<StockProvider>(context);
+    final stockList = provider.stocks
+        .where((element) =>
+            element.description.startsWith(provider.searchValue) ||
+            element.symbol.startsWith(provider.searchValue))
+        .toList();
     return ListView.separated(
         itemBuilder: (ctx, i) {
-          return Stock(
-            stockProvider.stocks[i].price,
-            stockProvider.stocks[i].symbol,
-            stockProvider.stocks[i].volume,
-            stockProvider.stocks[i].description,
-          );
+          return Stock(stockList[i]);
         },
         separatorBuilder: (context, index) {
           return Divider(
             color: Colors.grey[400],
           );
         },
-        itemCount: stockProvider.stocks.length);
-  }
-}
-
-class Stock extends StatelessWidget {
-  double price;
-  String symbol;
-  double volume;
-  String description;
-
-  Stock(this.price, this.symbol, this.volume, this.description);
-
-  @override
-  Widget build(BuildContext context) {
-    if (price == 0) {
-      return ListTile(
-        contentPadding: EdgeInsets.all(10),
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '${symbol}',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            Text(
-              '${description}',
-              style: TextStyle(
-                color: Colors.grey[500],
-                fontSize: 20,
-              ),
-            ),
-          ],
-        ),
-        trailing: CircularProgressIndicator(),
-      );
-    }
-    return ListTile(
-      contentPadding: EdgeInsets.all(10),
-      title: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '${symbol}',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          Text(
-            '${description}',
-            style: TextStyle(
-              color: Colors.grey[500],
-              fontSize: 20,
-            ),
-          ),
-        ],
-      ),
-      trailing: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Text(
-            '\$${price}',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          Container(
-            width: 75,
-            height: 20,
-            child: Text('+${volume}%',
-                style: TextStyle(
-                  color: Colors.white,
-                )),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              color: Colors.red,
-            ),
-          )
-        ],
-      ),
-    );
+        itemCount: stockList.length);
   }
 }
